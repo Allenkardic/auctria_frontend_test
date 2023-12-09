cf051742e3ef1b97b86bb09d24d9a736a253b6f0
work <work@email>
2023-12-08 23:35:55 -0500

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="p-5 text-lg text-center bg-green-50">
-    Welcome to Auctria's technical interview. Feel free to remove this component and start coding!
+    Welcome to Auctria's technical interview. Feel free to remove this component
+    and start coding! nfdjdj
   </div>
 </template>
